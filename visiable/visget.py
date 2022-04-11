2a4e4eddeb07cc297ce95ad007fa8124f16e2661
bitjungle <521520yjb>
2022-04-11 17:53:17 +0800

    Changes

--- visiable/visget.py
+++ visiable/visget.py
@@ -1,103 +1,72 @@
 # coding=utf-8
-
 from config import config
-from model import Transfer, Balance, Label
+from model import Transfer
 from utils import Date, Utils
-from visiable.viscut import Postcut, Precut, count, Nodecut
+from visiable.viscut import Postcut, count, Nodecut
 from visiable.vismodel import Edge, Node
-
-nodesappear: dict[str, list[set[Node]]] = {'from': [], 'to': []}
 
 
 class Get:
-    # 从数据库获取相关交易记录
-    @staticmethod
-    def get_nodes_set() -> set[str]:
-        ori = set(config.visnodes)
-        addresses = ori.copy()
+
+    # 爬取指定层数 获取所有边的集合
+    @classmethod
+    def get_nodes_edges(cls, from_or_to) -> tuple[list[list[Node]], list[Edge]]:
+        layers = []
+        edges = []
+
+        info = lambda x: (x[0], str(x[5]), x[3], x[4])
+        direction = lambda x: (x[1], x[2], info(x))
+
+        next_node = config.visnodes
+        layers.append(cls.create_node(next_node))
+
+        count[from_or_to] |= set(next_node)
+
         for _ in range(config.TURN):
-            res = Transfer.get_address(addresses)
-            for i in res:
-                address = i[0]
-                if not Label.get(address) and Balance.get(address):
-                    addresses.add(address)
-            print(len(addresses))
-        nodes = {}
-        edges = Transfer.get_transfer(list(addresses))
-        print(len(edges))
-        for i in edges:
-            edge = dict(zip(Transfer.column, i))
-            if edge['addrfrom'] not in nodes:
-                nodes[edge['addrfrom']] = Node(address=edge['addrfrom'], )
-            if edge['addrto'] not in nodes:
-                nodes[edge['addrto']] = Node(address=edge['addrto'], )
-            info = (edge['transferhash'], str(edge["blocktime"]), edge["symbol"], edge["value"])
-            nodes[edge['addrfrom']].add_edge(nodes[edge['addrto']], info)
-        return {v for k, v in nodes.items() if k in ori}
+            transfers = Transfer.get_transfer(next_node, from_or_to)
+            transfer = [direction(transfer) for transfer in transfers]
+            this_nodes, this_edges = cls.create_edge(transfer, from_or_to)
+            next_node = {node for node in this_nodes if not Nodecut(node, from_or_to).cut()}
 
-    # 爬取下一级节点集合
+            layers.append(cls.create_node(next_node))
+            edges.extend(this_edges)
+        return layers, edges
+
     @staticmethod
-    def get_next_nodes(node, edges_get, from_or_to) -> set[Node]:
-        next_nodes = set()
+    def create_node(all_node):
+        return [Node(node) for node in all_node]
 
-        for edge in node.edges_generate(from_or_to):
+    @staticmethod
+    def create_edge(all_edge, from_or_to):
+        pairs: dict[str, dict[str, Edge]] = dict()
+        edges = []
+        nodes = []
 
-            precut = Precut(edge, from_or_to)
-            if precut.cut():
-                continue
+        for edge in all_edge:
+            last, this = (edge[0], edge[1]) if from_or_to == 'to' else (edge[1], edge[0])
+            if last not in pairs:
+                pairs[last] = dict()
+            if this not in pairs[last]:
+                pairs[last].update({this: Edge(edge[0], edge[1])})
+            pairs[last][this].add_info(edge[2])
 
-            edges_get.append(edge)
-            if from_or_to == 'to':
-                edge.nodeto.to_relation = node.to_relation
-            else:
-                edge.nodefrom.from_relation = node.from_relation
-
-            postcut = Postcut(edge, from_or_to)
+        for address, pair in pairs.items():
+            postcut = Postcut(len(pair), address, Node.get_label(address))
             if postcut.cut():
                 continue
 
-            remote = edge.nodeto if from_or_to == 'to' else edge.nodefrom
-            next_nodes.add(remote)
-            count[from_or_to].add(remote)
+            edges.extend(pair.values())
+            nodes.extend(pair.keys())
 
-        return next_nodes
-
-    # 爬取指定层数 获取所有边的集合
-    @staticmethod
-    def get_edges(nodes_ori, from_or_to) -> list[Edge]:
-        nodes = nodes_ori.copy()
-        for node in nodes:
-            node.to_relation = {node}
-            node.from_relation = {node}
-            count[from_or_to].add(node)
-
-        edges_get: list[Edge] = []
-        nodesappear[from_or_to].append(nodes)
-        node_exits = set()
-        node_exits |= nodes
-        # while len(nodes) != 0:
-        for _ in range(config.TURN):
-            next_nodes = set()
-
-            for node in nodes:
-                nodecut = Nodecut(node, from_or_to)
-                if nodecut.cut():
-                    continue
-                next_nodes |= Get.get_next_nodes(node, edges_get, from_or_to)
-
-            nodes = next_nodes - node_exits
-            node_exits |= nodes
-            nodesappear[from_or_to].append(nodes)
-        return edges_get
+        return nodes, edges
 
 
 class Nodeinfo:
     __categories = {}
 
-    def __init__(self, node, from_or_to):
+    def __init__(self, node):
         self.node = node
-        self.hlen = node.to_hlen if from_or_to == 'to' else node.from_hlen
-        self.relation = node.to_relation if from_or_to == 'to' else node.from_relation
+        self.hlen = node.hlen
         self.color = None
 
     # tips中balance格式化
@@ -118,15 +87,6 @@ class Nodeinfo:
         form = "{{transferhash: {0} time: {1}  {2} {3} -> {4} {5}}}<br>"
         for i, inter in enumerate(internal):
             res += form.format(inter[1], inter[2], inter[3], inter[4], inter[5], inter[6])
-        return res
-
-    # tips中relation格式化
-    @staticmethod
-    def relationformat(relation) -> str:
-        res = str(len(relation)) + '<br>'
-        res += "from:<br>"
-        for relat in relation:
-            res += relat.address + '<br>'
         return res
 
     # 获取categories字典
@@ -166,9 +126,8 @@ class Nodeinfo:
     # 获取节点tips
     def get_node_tips(self) -> str:
         tips = self.node.address + '<br>'
-        tips += self.relationformat(self.relation) + \
-                self.balanceformat(self.node.balance) + \
-                self.internalformat(sorted(self.node.internal))
+        tips += self.balanceformat(self.node.balance)
+        # self.internalformat(sorted(self.node.internal))
         if self.color == 'blue':
             tips = self.node.label + '<br>' + tips
         tips = Utils.tip_filter(tips)
@@ -203,7 +162,7 @@ class Edgeinfo:
 
     # 获取边tips
     def get_edge_tips(self) -> str:
-        tips = "{0} -> {1}<br>".format(self.edge.nodefrom.address, self.edge.nodeto.address)
+        tips = "{0} -> {1}<br>".format(self.edge.nodefrom, self.edge.nodeto)
         form = "{{{1}, {2}: {3}, transferhash: {0}}}<br>"
         for info in sorted(self.edge.info):
             info = Utils.outof_list(info[1])
